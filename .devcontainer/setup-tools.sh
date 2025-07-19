da9f3a5e18d74ccba471eb67d0276b996cf186b0
joaqu1m <joaqu1m.pires@hotmail.com>
2025-07-19 21:50:46 +0000

    fixing protoc installation

--- .devcontainer/setup-tools.sh
+++ .devcontainer/setup-tools.sh
@@ -4,3 +4,5 @@ set -e
 go install -tags 'mysql' github.com/golang-migrate/migrate/v4/cmd/migrate@latest
 go install github.com/sqlc-dev/sqlc/cmd/sqlc@latest
 go install github.com/google/wire/cmd/wire@latest
+go install google.golang.org/protobuf/cmd/protoc-gen-go@latest
+go install google.golang.org/grpc/cmd/protoc-gen-go-grpc@latest
